a92f406502f494f3404e99ef2a3a0bf404cb8149
Maksym Skorupskyi <33811372+MaksymSkorupskyi@users.noreply.github.com>
2018-04-19 20:19:02 +0300

    Add files via upload

--- Math/Sum of Two Integers (371).py
+++ Math/Sum of Two Integers (371).py
@@ -5,8 +5,17 @@ Calculate the sum of two integers a and b, but you are not allowed to use the op
 Example:
 Given a = 1 and b = 2, return 3."""
 
+# a.__add__(b) Python 3 solution  [Runtime 32 ms beats 100.00 % of python3 submissions]
+class Solution:
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        return a.__add__(b)
 
-# Runtime: 32 ms beats 100.00 % of python3 submissions. :))))
+# sum([a, b]) Python 3 solution [Runtime: 32 ms beats 100.00 % of python3 submissions]
 class Solution:
     def getSum(self, a, b):
         """

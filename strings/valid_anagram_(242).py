e963a3125d2164fd4af710c1b553e9fe44822093
Maksym Skorupskyi <33811372+MaksymSkorupskyi@users.noreply.github.com>
2022-06-19 21:17:10 +0300

    Add files via upload

---
+++ strings/valid_anagram_(242).py
@@ -0,0 +1,70 @@
+"""242. Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+Constraints:
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+"""
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_memo = {}
+        t_memo = {}
+        for i, _ in enumerate(s):
+            if s[i] not in s_memo:
+                s_memo[s[i]] = 1
+            else:
+                s_memo[s[i]] += 1
+            if t[i] not in t_memo:
+                t_memo[t[i]] = 1
+            else:
+                t_memo[t[i]] += 1
+        return s_memo == t_memo
+
+
+# setdefault
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_memo = {}
+        t_memo = {}
+        for i, _ in enumerate(s):
+            s_memo[s[i]] = s_memo.setdefault(s[i], 1) + 1
+            t_memo[t[i]] = t_memo.setdefault(t[i], 1) + 1
+        return s_memo == t_memo
+
+
+# get
+# Runtime: 57 ms, faster than 78.48% of Python3 online submissions for Valid Anagram.
+# Memory Usage: 14.4 MB, less than 96.95% of Python3 online submissions for Valid Anagram.
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_memo = {}
+        t_memo = {}
+        for i, _ in enumerate(s):
+            s_memo[s[i]] = s_memo.get(s[i], 0) + 1
+            t_memo[t[i]] = t_memo.get(t[i], 0) + 1
+        return s_memo == t_memo

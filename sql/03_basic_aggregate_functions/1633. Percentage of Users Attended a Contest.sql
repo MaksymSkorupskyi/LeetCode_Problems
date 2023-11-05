14a810af8c770626de5b0928aae9d9e59848e965
Max Skorupskyi <maksym@relatus.com>
2023-11-05 14:37:44 +0200

    1211. Queries Quality and Percentage

--- sql/03_basic_aggregate_functions/1633. Percentage of Users Attended a Contest.sql
+++ sql/03_basic_aggregate_functions/1633. Percentage of Users Attended a Contest.sql
@@ -62,7 +62,8 @@
 -- | 207        | 33.33      |
 -- +------------+------------+
 -- Explanation:
--- All the users registered in contests 208, 209, and 210. The percentage is 100% and we sort them in the answer table by contest_id in ascending order.
+-- All the users registered in contests 208, 209, and 210.
+-- The percentage is 100% and we sort them in the answer table by contest_id in ascending order.
 -- Alice and Alex registered in contest 215 and the percentage is ((2/3) * 100) = 66.67%
 -- Bob registered in contest 207 and the percentage is ((1/3) * 100) = 33.33%
 SELECT

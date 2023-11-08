b5c05159ac308aa54d102320fe660813c3262a4d
Max Skorupskyi <maksym@relatus.com>
2023-11-08 19:41:34 +0200

    196. Delete Duplicate Emails

---
+++ sql/07_advanced_string_functions/196. Delete Duplicate Emails.sql
@@ -0,0 +1,51 @@
+-- 196. Delete Duplicate Emails
+-- Easy
+-- Table: Person
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains an email. The emails will not contain uppercase letters.
+--
+-- Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+-- For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+--
+-- After running your script, the answer shown is the Person table.
+-- The driver will first compile and run your piece of code and then show the Person table.
+-- The final order of the Person table does not matter.
+--
+-- Example 1:
+-- Input:
+-- Person table:
+-- +----+------------------+
+-- | id | email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- | 3  | john@example.com |
+-- +----+------------------+
+-- Output:
+-- +----+------------------+
+-- | id | email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- +----+------------------+
+-- Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+WITH
+  cte_unique_ids AS (
+    SELECT
+      MIN(id) AS id
+    FROM
+      Person
+    GROUP BY
+      email
+  )
+
+DELETE FROM
+  Person
+WHERE
+  id NOT IN (SELECT id FROM cte_unique_ids)

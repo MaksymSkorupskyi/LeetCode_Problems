b4ab57339bb7478ee4bbc638d0427b9e990e7054
Max Skorupskyi <maksym@relatus.com>
2023-11-11 12:52:51 +0200

    383. Ransom Note

---
+++ strings/383. Ransom Note.py
@@ -0,0 +1,55 @@
+"""383. Ransom Note
+Easy
+Given two strings ransomNote and magazine, 
+return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Example 3:
+Input: ransomNote = "aa", magazine = "aab"
+Output: true 
+
+Constraints:
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
+"""
+
+
+class Solution:
+    @staticmethod
+    def canConstruct(ransom_note: str, magazine: str) -> bool:
+        ransom_counter = {}
+        for s in ransom_note:
+            ransom_counter[s] = ransom_counter.setdefault(s, 0) + 1
+
+        magazine_counter = {}
+        for s in magazine:
+            magazine_counter[s] = magazine_counter.setdefault(s, 0) + 1
+
+        print(ransom_counter)
+        print(magazine_counter)
+
+        for k, v in ransom_counter.items():
+            if k not in magazine_counter or magazine_counter[k] < v:
+                return False
+
+        return True
+
+
+test_cases = (
+    ("a", "b", False),
+    ("aa", "ab", False),
+    ("aa", "aab", True),
+    ("bg", "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj", True),
+)
+for x, y, answer in test_cases:
+    result = Solution.canConstruct(x, y)
+    print(result)
+    assert result == answer
